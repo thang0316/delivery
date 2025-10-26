105907fad1f6b982d61bbea2b4c3ff414efe0906
thang0316 <147375854+thang0316@users.noreply.github.com>
2025-10-26 17:49:59 +0700

    xóa custom

--- src/main/java/com/fooddelivery/delivery/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fooddelivery.delivery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.fooddelivery.delivery.entity.Customer;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, String> {
-}
