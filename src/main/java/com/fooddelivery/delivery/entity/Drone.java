6e0d9ecfea6b0a1b3c7ec158e539eb4e3685d3f8
thang0316 <147375854+thang0316@users.noreply.github.com>
2025-10-28 23:56:58 +0700

    review

--- src/main/java/com/fooddelivery/delivery/entity/Drone.java
+++ src/main/java/com/fooddelivery/delivery/entity/Drone.java
@@ -1,11 +1,9 @@
 package com.fooddelivery.delivery.entity;
 
 import jakarta.persistence.*;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 @Entity
+@Table(name = "drones")
 public class Drone {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
--- src/main/java/com/fooddelivery/delivery/entity/MenuItem.java
+++ src/main/java/com/fooddelivery/delivery/entity/MenuItem.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "menu_items")
 public class MenuItem {
 
 	@Id
