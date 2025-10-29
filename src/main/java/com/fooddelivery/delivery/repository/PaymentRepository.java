2cbf258b25c2c437644e02bd3666b697ed4a880c
thang0316 <147375854+thang0316@users.noreply.github.com>
2025-10-29 22:38:17 +0700

    payment

---
+++ src/main/java/com/fooddelivery/delivery/repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.fooddelivery.delivery.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fooddelivery.delivery.entity.Payment;
+
+public interface PaymentRepository extends JpaRepository<Payment, String> {
+	List<Payment> findByOrderId(Long orderId);
+    List<Payment> findByStatus(String status);
+}
