e2acd2ec3cd5ce1e1e73cab7f2fc9adf9acaf691
thang0316 <147375854+thang0316@users.noreply.github.com>
2025-11-03 10:49:59 +0700

    Xóa toàn bộ file khỏi Git tracking

--- src/main/java/com/fooddelivery/delivery/entity/RoleName.java
+++
@@ -1,7 +0,0 @@
-package com.fooddelivery.delivery.entity;
-
-public enum RoleName {
-    ADMIN,        
-    RESTAURANT,   
-    CUSTOMER      
-}
--- src/main/java/com/fooddelivery/delivery/repository/RestaurantRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fooddelivery.delivery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.fooddelivery.delivery.entity.Restaurant;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant, String> {
-    
-}
