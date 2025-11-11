a3327d8e3cc62743263a43ea3c61b42db02f3bac
thang0316 <147375854+thang0316@users.noreply.github.com>
2025-11-11 08:48:10 +0700

    đồng bộ tổng tiền với ORDER

--- src/main/java/com/fooddelivery/delivery/dto/request/PaymentRequest.java
+++ src/main/java/com/fooddelivery/delivery/dto/request/PaymentRequest.java
@@ -3,7 +3,7 @@ package com.fooddelivery.delivery.dto.request;
 public class PaymentRequest {
 	private Long orderId;
     private String method;   // Hình thức thanh toán
-    private Double amount;   // Số tiền
+    
     private String status;   // Trạng thái ("PENDING", "SUCCESS", "FAILED")
     
 	public Long getOrderId() {
@@ -17,12 +17,6 @@ public class PaymentRequest {
 	}
 	public void setMethod(String method) {
 		this.method = method;
-	}
-	public Double getAmount() {
-		return amount;
-	}
-	public void setAmount(Double amount) {
-		this.amount = amount;
 	}
 	public String getStatus() {
 		return status;
